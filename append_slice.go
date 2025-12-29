c017e6e20984106cf3df589e8ff25ebb2117064d
annasnurdin <anasnurdin02@gmail.com>
2025-12-29 17:11:24 +0700

    append slice

--- append_slice.go
+++ append_slice.go
@@ -9,4 +9,10 @@ func main(){
 
 	slice2 := append(sliceku, 5, 6)
 	fmt.Println("slice 2 =", slice2)
+
+	xx := []int {1,2,3}
+	uu := []int {3,4,5,6}
+
+	cc := append(xx, uu...)
+	fmt.Println("append 2 slice =", cc)
 }
