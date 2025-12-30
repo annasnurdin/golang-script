5f3dd719c7c05290f1fdd99948c990722b475b29
annasnurdin <anasnurdin02@gmail.com>
2025-12-30 17:04:35 +0700

    fmt

---
+++ standard-library/fmt.go
@@ -0,0 +1,11 @@
+package main
+
+import "fmt"
+
+func main() {
+	firstname := "Annas"
+	lastname := "XX"
+
+	fmt.Println("HELLOW", firstname, lastname)
+	fmt.Printf("Hellow %s %s", firstname, lastname)
+}
