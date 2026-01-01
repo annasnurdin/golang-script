bdbb44f5ad360c5c503fda90179d99ba600bed66
annasnurdin <anasnurdin02@gmail.com>
2026-01-01 08:48:04 +0700

    math

---
+++ standard-library/math.go
@@ -0,0 +1,13 @@
+package main
+
+import (
+	"fmt"
+	"math"
+)
+
+func main() {
+	fmt.Println(math.Ceil(1.12))
+	fmt.Println(math.Floor(1.99))
+	fmt.Println(math.Round(1.51))
+	fmt.Println(math.Max(12, 13))
+}
