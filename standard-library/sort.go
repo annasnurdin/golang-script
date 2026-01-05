71da2fa392d919188e93f882a40d259436dbf88d
annasnurdin <anasnurdin02@gmail.com>
2026-01-05 08:28:31 +0700

    sort

---
+++ standard-library/sort.go
@@ -0,0 +1,46 @@
+package main
+
+import (
+	"fmt"
+	"sort"
+)
+
+type User struct {
+	Name string
+	Age  int
+}
+
+type UserSlice []User
+
+func (s UserSlice) Len() int {
+	return len(s)
+}
+
+func (s UserSlice) Less(i, j int) bool {
+	return s[i].Age < s[j].Age
+}
+
+func (s UserSlice) Swap(i, j int) {
+	// temp := s[i]
+	// s[i] = s[j]
+	// s[j] = temp
+	s[i], s[j] = s[j], s[i]
+}
+
+func main() {
+	users := []User{
+		{"Annas", 21},
+		{"Purwo", 22},
+		{"Karin", 19},
+	}
+
+	sort.Sort(UserSlice(users))
+	fmt.Println(users)
+
+	test := []User{
+		{"Annas", 10},
+		{"Purwo", 15},
+	}
+	sort.Sort(UserSlice(test))
+	fmt.Println(test)
+}
